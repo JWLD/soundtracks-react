207f3abdc476894c33e26f5622e2e62859580ea6
JWLD <jackwldavies@icloud.com>
2017-06-13 13:55:31 +0300

    show reset button when input contains text

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import FaClose from 'react-icons/lib/fa/close';
 
 class SearchBar extends Component {
   // assign initial state
   constructor(props) {
     super(props);
 
-    this.state = { term: '' };
+    this.state = {
+      term: '',
+      className: 'fa fa-close page-hdr__reset page-hdr__reset_hide'
+    };
 
     // bind onInputChange to this class
     this.onInputChange = this.onInputChange.bind(this);
@@ -13,16 +17,27 @@ class SearchBar extends Component {
 
   onInputChange(event) {
     this.setState({ term: event.target.value });
+
+    if (event.target.value) {
+      this.setState({ className: 'fa fa-close page-hdr__reset' });
+    } else {
+      this.setState({ className: 'fa fa-close page-hdr__reset page-hdr__reset_hide' });
+    }
   }
 
   render() {
     return (
-      <input
-        className="page-hdr__input"
-        value={this.state.term}
-        onChange={this.onInputChange}
-        placeholder="Search for Composer"
-      />
+      <div>
+        <input
+          className='page-hdr__input'
+          value={this.state.term}
+          onChange={this.onInputChange}
+          placeholder='Search for Composer'
+        />
+        <div className='page-hdr__reset-wrap'>
+          <FaClose className={this.state.className} />
+        </div>
+      </div>
     );
   }
 };
