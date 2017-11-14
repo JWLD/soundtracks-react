8248f1140e27b356c351ee114cff0a34e2295c50
JWLD <jackwldavies@icloud.com>
2017-11-14 16:13:22 +0000

    refactor artist and album list into component state

--- app/components/AlbumList.jsx
+++ app/components/AlbumList.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Axios from 'axios';
-
-import SearchBar from './SearchBar';
-import { setAlbums } from '../redux/actions';
+const Querystring = require('querystring');
 
 class AlbumList extends Component {
-  componentWillMount() {
-    this.props.getAlbumsByArtist(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      albums: []
+    };
+  }
+
+  componentDidMount() {
+    Axios.get(`http://localhost:9000/albums?q=${this.props.match.params.id}`).then((response) => {
+      this.setState({ albums: response.data });
+    });
   }
 
   render() {
-    const albumList = this.props.albums
+    const albumList = this.state.albums
       .filter(album => {
         return album.title.toLowerCase().indexOf(this.props.searchTerm.toLowerCase()) >= 0 && album.spotify_img
       }).map(album => {
@@ -43,18 +51,7 @@ class AlbumList extends Component {
 };
 
 const mapStateToProps = (state) => ({
-  searchTerm: state.searchTerm,
-  albums: state.albums
+  searchTerm: state.searchTerm
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  getAlbumsByArtist: (id) => {
-    dispatch(setAlbums([])); // clear albums before loading
-
-    Axios.get(`http://localhost:9000/albums?q=${id}`).then((response) => {
-      dispatch(setAlbums(response.data));
-    });
-  }
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(AlbumList);
+export default connect(mapStateToProps)(AlbumList);
--- app/redux/actions.js
+++ app/redux/actions.js
@@ -1,8 +1,6 @@
 // ACTION TYPES
 
 export const SET_SEARCH_TERM = 'SET_SEARCH_TERM';
-export const SET_ARTISTS = 'SET_ARISTS';
-export const SET_ALBUMS = 'SET_ALBUMS';
 
 // ACTION CREATORS
 
@@ -11,18 +9,4 @@ export function setSearchTerm(searchTerm) {
     type: SET_SEARCH_TERM,
     payload: searchTerm
   };
-}
-
-export function setArtists(artists) {
-  return {
-    type: SET_ARTISTS,
-    payload: artists
-  }
-}
-
-export function setAlbums(albums) {
-  return {
-    type: SET_ALBUMS,
-    payload: albums
-  }
 }
