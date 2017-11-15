00c25df8c09c9649c2850d5c3d20e0290aeacc5c
JWLD <jackwldavies@icloud.com>
2017-11-15 17:02:48 +0000

    move inside li tags

--- app/components/AlbumTile.jsx
+++ app/components/AlbumTile.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 const AlbumTile = (props) => (
-  <a
-    className="results-sctn__result-tile"
-    key={props.id}
-    style={props.tileStyle}
-    href={props.link}
-    target="_blank"
-  ><div className="results-sctn__result-hover">{props.title}</div>
-  </a>
+  <li>
+    <a
+      className="results-sctn__result-tile"
+      key={props.id}
+      style={props.tileStyle}
+      href={props.link}
+      target="_blank"
+      ><div className="results-sctn__result-hover">{props.title}</div>
+    </a>
+  </li>
 );
 
 export default AlbumTile;
