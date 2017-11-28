622bc83c82c6241066f17a09135d0c3f3aeff41c
JWLD <jackwldavies@icloud.com>
2017-11-28 22:49:04 +0000

    restructure project files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './app/App.jsx',
+  entry: './src/client/App.jsx',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
